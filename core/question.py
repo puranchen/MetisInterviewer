09af2da3a8fed87eb880806daf5bf695240bbfd7
Puran Chen <puran.chen@platform24.com>
2024-06-08 22:16:15 +0200

    refactor: improved ask method

--- core/question.py
+++ core/question.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Union, List
 
 class QuestionABC(ABC):
     """ Base class for all questions. """
@@ -14,9 +15,9 @@ class QuestionABC(ABC):
     def prompt(self):
         return self._prompt
     
-    @abstractmethod
-    def set_prompt(self, value):
-        pass
+    def set_prompt(self, prompt, lang):
+        """ used for setting prompts in other languages """
+        self._prompt.update({lang: prompt})
 
     @property
     def answer(self):
@@ -25,52 +26,95 @@ class QuestionABC(ABC):
     @abstractmethod
     def set_answer(self, value=None):
         pass
-    
-
 
 class QuestionBool(QuestionABC):
     """ Question with a boolean answer. """
     def set_answer(self, value=None):
+        """ Set the answer to the question. """
         if value is not None:
             if not isinstance(value, bool) and not value in [0,1]:
                 raise ValueError(f"Answer must be a boolean, got {type(value)}:")
             self._answer = bool(value)
     
-    def set_prompt(self, value):
-        self._prompt.update(value)
+    def set_prompt(self, prompt, lang = 'en'):
+        """ used for setting prompts in other languages """
+        self._prompt.update({lang: prompt})
 
     def ask(self, lang='sv'):
-        answer = ""
-        while answer not in [1, 2]:
-            print("")
-            print(self.prompt.get(lang))
-            print("1 - Ja\n2 - Nej")
-            answer = int(input("Svar: "))
-        self.set_answer(answer == 1)
-        self.asked = True
+        """ used for debugging and testing, default language is Swedish """
+        while True:
+            print("\n", self.prompt.get(lang), sep="")
+            answer_options = {"sv": "1 - Ja\n2 - Nej", "en": "1 - Yes\n2 - No"}
+            print(answer_options[lang])
+            answer = input("Svar: ")
+            if self._is_valid_user_input(answer):
+                self.set_answer(answer == "1")
+                self.asked = True
+                break
+            else:
+                print("Invalid answer. Please try again.")
 
+    def _is_valid_user_input(self, answer):
+        """ Helper function to check if user input is a valid boolean. """
+        return answer in ["1", "2"]
+    
     def __repr__(self):
         return f"QuestionBool(prompt={self.prompt!r}, answer={self._answer!r})"
 
 class QuestionInt(QuestionABC):
     """ Question with an integer answer. """
+
+    def __init__(self, prompt, lang = 'en', min_value:int=None, max_value:int=None, skippable=False):
+        super().__init__(prompt, lang=lang, skippable=skippable)
+        self.max_value = max_value
+        self.min_value = min_value
+
     def set_answer(self, value=None):
+        """ Set the answer to the question. """
         if value is not None:
             if not isinstance(value, int):
                 raise ValueError(f"Answer must be an integer, got {type(value)}:")
-            self._answer = int(value)
-    
-    def set_prompt(self, value):
-        self._prompt.update(value)
+            self._answer = value
 
     def ask(self, lang='sv'):
-        answer = None
-        while not answer.isdigit():
-            print("")
-            print(self.prompt.get(lang))
-            answer = input("Svar: ")
-        self.set_answer(int(answer))
-        self.asked = True
+        """ used for debugging and testing """
+        while True:
+            # Print to console and get user input
+            print("\n", self.prompt.get(lang), sep="")
+            answer = input(f"Answer ({self.unit}): ").replace(",",".")
+
+            # Check if user input is valid
+            if self._is_valid_user_input(answer):
+                self.set_answer(int(answer))
+                self.asked = True
+                break
+            else:
+                self._print_invalid_message(answer)
+
+    def _is_valid_user_input(self, answer):
+        """ Helper function to check if user input is a valid integer. """
+        try:
+            value = int(answer)
+        except ValueError:
+            return False
+        
+        if self.max_value and value > self.max_value:
+            return False
+        if self.min_value and value < self.min_value:
+            return False
+        
+        return True
+    
+    def _print_invalid_message(self, answer):
+        """ Helper function to print an error message when user input is invalid. """
+        if self.min_value is not None and self.max_value is not None:
+            print(f"Invalid answer: {answer!r}. Must be a number between {self.min_value!r} and {self.max_value!r}. Please try again.")
+        elif self.min_value is not None:
+            print(f"Invalid answer: {answer!r}. Must be a number larger than {self.min_value!r}. Please try again.")
+        elif self.max_value is not None:
+            print(f"Invalid answer: {answer!r}. Must be a number smaller than {self.max_value!r}. Please try again.")
+        else:
+            print(f"Invalid answer: {answer!r}. Must be a number, please try again.")
 
     def __repr__(self):
         return f"QuestionInt(prompt={self.prompt!r}, answer={self._answer!r})"
@@ -84,39 +128,54 @@ class QuestionFloat(QuestionABC):
         self.max_value = max_value
         self.min_value = min_value
 
-    def set_answer(self, value=None):
+    def set_answer(self, value: Union[float, int]=None):
         if value is not None:
-            if not isinstance(value, (float, int)):
+            if not isinstance(value, (float, int)): # Accept floats and ints (typecasts to float)
                 raise ValueError(f"Answer must be a float or int, got {type(value)}:")
-            self._answer = float(value)
+            else: # Check if value within boundaries when applicable
+                if self.min_value and value < self.min_value:
+                    raise ValueError(f"Answer must be greater than {self.min_value}")
+                if self.max_value and value > self.max_value:
+                    raise ValueError(f"Answer must be less than {self.max_value}")
+            self._answer = float(value) 
     
-    def set_prompt(self, value):
-        self._prompt.update(value)
-
     def ask(self, lang='sv'):
-        answer = ""
-        while not answer.replace(".", "").replace(",","").isdigit():
-            print("")
-            print(self.prompt.get(lang))    
-            answer = input(f"Svar ({self.unit}): ")
-            if not answer.replace(".", "").replace(",","").isdigit():
-                print(f"Invalid answer: {answer!r}. Must be a number.")
-                continue
-            outside_limits = False
-            if self.max_value is not None:
-                if float(answer) > self.max_value:  
-                    print(f"Value too large: {answer}. Must be a between {self.min_value!r} and {self.max_value!r}.")
-                    outside_limits = True
-            if self.min_value is not None:
-                if float(answer) < self.min_value:
-                    print(f"Value too small: {answer}. Must be a between {self.min_value!r} and {self.max_value!r}.")
-                    outside_limits = True
-            if outside_limits:
-                answer = ""
-                continue
+        while True:
+            # Print to console and get user input
+            print("\n", self.prompt.get(lang), sep="")
+            answer = input(f"Answer ({self.unit}): ").replace(",",".")
 
-        self.set_answer(float(answer.replace(",",".")))
-        self.asked = True
+            # Check if user input is valid
+            if self._is_valid_user_input(answer):
+                self.set_answer(float(answer))
+                self.asked = True
+                break
+            else:
+                self._print_invalid_message(answer)
+
+    def _is_valid_user_input(self, answer):
+        try:
+            value = float(answer)
+        except ValueError:
+            return False
+        
+        if self.max_value and value > self.max_value:
+            return False
+        if self.min_value and value < self.min_value:
+            return False
+        
+        return True
+    
+    def _print_invalid_message(self, answer):
+        """ Helper function to print an error message when user input is invalid. """
+        if self.min_value is not None and self.max_value is not None:
+            print(f"Invalid answer: {answer!r}. Must be a number between {self.min_value!r} and {self.max_value!r}. Please try again.")
+        elif self.min_value is not None:
+            print(f"Invalid answer: {answer!r}. Must be a number larger than {self.min_value!r}. Please try again.")
+        elif self.max_value is not None:
+            print(f"Invalid answer: {answer!r}. Must be a number smaller than {self.max_value!r}. Please try again.")
+        else:
+            print(f"Invalid answer: {answer!r}. Must be a number, please try again.")
 
     def __repr__(self):
         return f"QuestionFloat(prompt={self.prompt!r}, answer={self._answer!r})"
@@ -127,12 +186,8 @@ class QuestionStr(QuestionABC):
         if value is not None:
             self._answer = str(value)
     
-    def set_prompt(self, value):
-        self._prompt.update(value)
-
     def ask(self, lang='sv'):
-        print("")
-        print("\n", self.prompt.get(lang))
+        print("\n", self.prompt.get(lang), sep="")
         answer = input("Svar: ")
         self.set_answer(answer)
         self.asked = True
@@ -151,124 +206,116 @@ class MCOption(QuestionBool):
 
 class MultipleChoice(QuestionABC):
     """ Question with multiple choice answers. """
-    def __init__(self, prompt:str, choices:list, answer:list=None, lang:str='en', none_option=True, none_prompt="None of the above", mc_type='single-select', answered=False):
+    def __init__(self, prompt:str, choices:list, answer:list=None, lang:str='en', none_option=True, none_prompt="None of the above", variant='single-select', answered=False):
         super().__init__(prompt, answer, lang)
         self._choices = []
         self.none_option = none_option
-        self.none_prompt = none_prompt
-        self.mc_type = mc_type
-        self._answered = False
+        self.variant = variant
         if answered:
             self.complete()
-        self.set_choices(choices, lang)
+        self.set_choices(choices, lang, none_prompt)
         self._answer: list = []
         
     @property
     def choices(self):
         return self._choices
     
-    def set_choices(self, choices: list, lang:str='en'):
+    def set_choices(self, choices: list, lang:str='en', none_prompt="None of the above"):
         if all(isinstance(choice, str) for choice in choices):
             self._choices = [MCOption(idx+1, prompt=prompt, answer=None, lang=lang) for idx, prompt in enumerate(choices)]
             if self.none_option:
-                self._choices.append(MCOption(0, prompt=self.none_prompt, lang=lang))
+                self._choices.append(MCOption(0, prompt=none_prompt, lang=lang))
 
         elif all(isinstance(choice, MCOption) for choice in choices):
             self._choices = choices
         else:
             raise ValueError("Choices must be a list of strings or QuestionBool objects.")
 
-    def set_answer(self, value=None):
-        if isinstance(value, int) or (isinstance(value, str) and value.isdigit()):
-            value = int(value)
-            if value == 0:
-                self.reset_answers()
-                for choice in self._choices:
-                    choice.set_answer(False)
-                    if choice.idx == 0:
-                        choice.set_answer(True)
-                        self._answer.append(choice)
-            else: # if answer is not 'None of the above'
-                for choice in self._choices:
-                    if choice.idx == value: # Append the choice to self._answer
-                        choice.set_answer(True)
-                        self._answer.append(choice)
-                    elif choice.idx == 0 and value is not None: # Set 'None of the above' to False
-                        choice.set_answer(False) 
-                    else:
-                        if self.mc_type == 'single-select': # Set all other choices to False if single-select
-                            choice.set_answer(False)
-        
-    def set_prompt(self, value):
-        self._prompt.update(value)
+    def set_answer(self, value: Union[int, List[int]]=None):
+        assert type(value) == int
+
+        if value == 0:
+            self.reset_answers()
+            for choice in self._choices:
+                choice.set_answer(False)
+                if choice.idx == 0:
+                    choice.set_answer(True)
+                    self._answer.append(choice)
+        else: # if answer is not 'None of the above'
+            for choice in self._choices:
+                if choice.idx == value: # Append the choice to self._answer
+                    choice.set_answer(True)
+                    self._answer.append(choice)
+                elif choice.idx == 0 and value is not None: # Set 'None of the above' to False
+                    choice.set_answer(False) 
+                else:
+                    if self.variant == 'single-select': # Set all other choices to False if single-select
+                        choice.set_answer(False)
 
     @property
     def answer(self):
-        if self.mc_type == 'single-select':
+        if self.variant == 'single-select':
             return self._answer
-        elif self.mc_type == 'multi-select':
+        elif self.variant == 'multi-select':
             if len(self._answer) == 0:
                 return []
             else:
                 return self._answer
     
     def reset_answers(self):
+        """ Reset the answer to the question. """
         for choice in self.choices:
             choice.set_answer(None)
         self._answer = []
 
-    def print_question(self, lang='sv'):
-        print("")
-        print(self.prompt.get(lang))
+    def print_question(self, lang):
+        print("\n", self.prompt.get(lang), sep="")
         for choice in self.choices:
             print(f"{choice.idx} - {choice.prompt.get(lang)}")
 
     def ask(self, lang='sv'):
         
         answer_lst = []
-        
-        if self.mc_type == 'single-select':
-            while len(answer_lst) == 0:
-                self.print_question(lang)
-                raw_answer = input("Svar: ")
-                answer_lst = raw_answer.split(",")
-                if len(answer_lst) == 1:
-                    try:
-                        int_answer = int(answer_lst[0])
-                        if int_answer in [opt.idx for opt in self.choices]:
-                            self.set_answer(int_answer)
-                            break
-                    except:
-                        pass
-                print(f"Invalid answer: {raw_answer!r}, please try again.\n")
-                answer_lst = []
+        self.asked = False
 
-        elif self.mc_type == 'multi-select':
-            list_answer = [""]
+        while True:
+            self.reset_answers()
+            self.print_question(lang)
+            answer = input("Svar: ")
+
+            if self.variant == 'single-select':
+                if answer.isdigit():
+                    int_answer = int(answer)
+                    if int_answer in [opt.idx for opt in self.choices]:
+                        self.set_answer(int_answer)
+                        self.complete()
+                        break
+                print(f"Invalid answer: {answer!r}, please try again.\n")
+                continue
             
-            while not all(a.strip().isdigit() and int(a.strip()) in [c.idx for c in self.choices] for a in list_answer):
-                self.reset_answers()
-                self.print_question(lang)
-                raw_answer = input("Svar: ")
-                list_answer = raw_answer.split(",")    
-                int_lst = [int(a.strip()) for a in list_answer]
-            
-            if any(i==0 for i in int_lst):
-                self.set_answer(0)
-            else:
-                for i in int_lst:
-                    self.set_answer(i)
-            
-        self.complete()
+            elif self.variant == 'multi-select':
+                answer_lst = [a.strip() for a in answer.split(",")]
+                if all(a.isdigit() for a in answer_lst):
+                    if all(int(a) in [c.idx for c in self.choices] for a in answer_lst):
+                        answer_lst = [int(a) for a in answer_lst]
+                        if any(i==0 for i in answer_lst):
+                            self.set_answer(0)
+                        else:
+                            for i in answer_lst:
+                                self.set_answer(i)
+                        self.complete()
+                        break
+                print(f"Invalid answer: {answer!r}, please try again.\n")
+                continue
 
     def complete(self):
-        self._answered = True
         if all(choice.answer == None for choice in self.choices):
             self.set_answer(0)
             return
         for choice in self.choices:
             if choice.answer is None:
                 choice.set_answer(False)
+        self.asked = True
 
     def __repr__(self):
         return f"MultipleChoice(prompt={self.prompt!r}, choices={self._choices!r})"
